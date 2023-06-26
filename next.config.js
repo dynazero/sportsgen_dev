83d761dddfbb58326b3c811f3720c40a32205a22
jamesgarcia <jamesgarciadev@gmail.com>
2023-06-26 21:56:25 +0800

    config.module.rules and config.externals added

--- next.config.js
+++ next.config.js
@@ -1,19 +1,32 @@
 module.exports = {
   images: {
-    domains: ['sportsgenph.sgp1.digitaloceanspaces.com','sportsgenph.sgp1.cdn.digitaloceanspaces.com'],
+    domains: ['sportsgenph.sgp1.digitaloceanspaces.com', 'sportsgenph.sgp1.cdn.digitaloceanspaces.com'],
   },
   webpack: (config, { isServer }) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.resolve.fallback = {
-          fs: false,
-          net: false,
-          tls: false,
-          dns: false
-        }
-      }
-  
-      return config
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+        dns: false,
+      };
+
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      });
+
+      config.externals = {
+        ...config.externals,
+        kerberos: 'commonjs kerberos',
+        '@mongodb-js/zstd': 'commonjs @mongodb-js/zstd',
+        '@aws-sdk/credential-providers': 'commonjs @aws-sdk/credential-providers',
+        snappy: 'commonjs snappy',
+        aws4: 'commonjs aws4',
+        'mongodb-client-encryption': 'commonjs mongodb-client-encryption'
+      };
     }
-  }
-  
+
+    return config;
+  },
+};
