7e4532c1beef31450f88048d6a252820a7c368f1
jamesgarcia <jamesgarciadev@gmail.com>
2023-06-28 12:28:22 +0800

    route update [...nextauth.js]

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,17 +7,17 @@ import clientPromise from "../../../lib/mongodb";
 
 const NEXT_PUBLIC_APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
 
-let NEXTAUTH_URL_DASHBOARD;
+let NEXTAUTH_URL;
 
 switch (NEXT_PUBLIC_APP_ENV) {
   case 'dev':
-    NEXTAUTH_URL_DASHBOARD = process.env.DEV_NEXTAUTH_URL_DASHBOARD;
+    NEXTAUTH_URL = process.env.DEV_NEXTAUTH_URL;
     break;
   case 'test':
-    NEXTAUTH_URL_DASHBOARD = process.env.NGROK_NEXTAUTH_URL_DASHBOARD;
+    NEXTAUTH_URL = process.env.NGROK_NEXTAUTH_URL;
     break;
   case 'production':
-    NEXTAUTH_URL_DASHBOARD = process.env.NEXTAUTH_URL_DASHBOARD;
+    NEXTAUTH_URL = process.env.NEXTAUTH_URL;
     break;
   default:
     console.error('Invalid environment specified in NEXT_PUBLIC_APP_ENV');
@@ -25,7 +25,7 @@ switch (NEXT_PUBLIC_APP_ENV) {
 }
 
 export const authOptions = {
-  url: NEXTAUTH_URL_DASHBOARD,
+  url: NEXTAUTH_URL,
   secret: process.env.AUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
@@ -38,12 +38,15 @@ export const authOptions = {
   ],
   callbacks: {
     async redirect({ url, baseUrl }) {
-      if (url === "/") {
+      console.log("URL before redirect:", url);
+      const path = new URL(url).pathname;
+      
+      if (path === "/") {
         // If user is logging in from the homepage, redirect to dashboard
         return `${baseUrl}/dashboard`;
-      } else if (url.startsWith("/")) {
+      } else if (path.startsWith("/")) {
         // For other relative URLs, return the URL as is
-        return `${baseUrl}${url}`;
+        return `${baseUrl}${path}`;
       } else if (new URL(url).origin === baseUrl) {
         // If the URL is on the same origin, return the URL as is
         return url;
