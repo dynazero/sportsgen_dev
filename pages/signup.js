59c7d7b7bb86c6bcce5aaca64962561c93edc304
jamesgarcia <jamesgarciadev@gmail.com>
2024-02-13 19:04:40 +0800

    server test, signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,42 +1,9 @@
-import { Datepicker } from '@adibfirman/react-datepicker'
 
-export default function signup({ res }) {
+export default function signup() {
   return (
     <div>
-      {res}
+      TEST PAGE
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-  const NEXT_PUBLIC_APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
-
-  let NEXT_PUBLIC_API_URL;
-
-  switch (NEXT_PUBLIC_APP_ENV) {
-    case 'dev':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_DEV_API_URL;
-      break;
-    case 'test':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_NGROK_API_URL;
-      break;
-    case 'production':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
-      break;
-    default:
-      console.error('Invalid environment specified in NEXT_PUBLIC_APP_ENV');
-      break;
-  }
-  // Fetch data from APIs
-  const apiUrl = NEXT_PUBLIC_API_URL;
-  const getLaterEventsEndPoint = "/api/getLaterEvents"
-
-  const res = await fetchData(`${apiUrl}${getLaterEventsEndPoint}`);
-
-
-  return {
-    props: {
-      res,
-    }
-  };
-}
