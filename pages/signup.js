b505717db1a295d9d4f92b088527de08ee332281
jamesgarcia <jamesgarciadev@gmail.com>
2024-02-13 23:48:15 +0800

    server test, signup env status

--- pages/signup.js
+++ pages/signup.js
@@ -9,24 +9,10 @@ export default function signup({ res }) {
 }
 
 export async function getServerSideProps(context) {
-  const NEXT_PUBLIC_APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;
 
-  let NEXT_PUBLIC_API_URL;
+  NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-  switch (NEXT_PUBLIC_APP_ENV) {
-    case 'dev':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_DEV_API_URL;
-      break;
-    case 'test':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_NGROK_API_URL;
-      break;
-    case 'production':
-      NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
-      break;
-    default:
-      console.error('Invalid environment specified in NEXT_PUBLIC_APP_ENV');
-      break;
-  }
+  
   // Fetch data from APIs
   const apiUrl = NEXT_PUBLIC_API_URL;
   const getLaterEventsEndPoint = "/api/getLaterEvents"
